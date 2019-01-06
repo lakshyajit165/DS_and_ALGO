a9e9c1183d76112663f3518cc31a7d6c68e96fbe
lakshyajit165 <lakshyajit165@gmail.com>
2019-01-06 23:38:54 +0530

    Practice GFG

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,12 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main(){
+
+int main()
+{
+    string s1 = "Fork";
+    string s2  = "cCPP";
+    string s3(s2, 1, 3);  // line 1
     
-    int a = 5,b = 9;
-    cout<<a<<" "<<b;    
+    cout << s1 << " " << s3;
     return 0;
-}
+} 
